26f6295c1b7023726fcdfc89a54513cb3b42ce07
Levi <levicarlson123@gmail.com>
2020-05-23 15:38:45 -0500

    monthly cost addition works

--- client.js
+++ client.js
@@ -10,13 +10,15 @@ function ready(){
   });
 }
 
+let arrEmployee = [];
+
 function addEmployee(){
   // declares variables with the value of the text inputs
   let first = $('#first').val();
   let last = $('#last').val();
-  let id = $('#id').val();
+  let id = Number($('#id').val());
   let title = $('#title').val();
-  let salary = $('#salary').val();
+  let salary = Number($('#salary').val());
   // declares variables with elements to be appended
   let elTr = $('<tr></tr>');
   let elFirst = $(`<td>${first}</td>`);
@@ -25,6 +27,14 @@ function addEmployee(){
   let elTitle = $(`<td>${title}</td>`);
   let elSalary = $(`<td>${salary}</td>`);
   let elDelBtn = $(`<td><button class="delete">Delete</button></td>`);
+
+  arrEmployee.push({
+    first: first,
+    last: last,
+    id: id,
+    title: title,
+    salary: salary
+  });
 
   // I love this if statement
   if(!first || !last || !id || !title || !salary){
@@ -45,23 +55,33 @@ function addEmployee(){
   $('#id').val('');
   $('#title').val('');
   $('#salary').val('');
-
-  $('.delete').on('click', function(event){
-    delEl = event.target;
+  
+  $('.delete').on('click', function (event) {
     delEmployee();
   });
+
+  refreshMonthlyCost();
 }
 
-// function updateCost(){
-//   let monthlyCost = 0;
-//   for(let i of $('thead tr')){
-//     let salaryMonthly = Number($(`thead ${i}:nth-child(4)`).val()) / 12;
-//     monthlyCost += salaryMonthly;
-//     $('#monthly').empty();
-//     $('#monthly').text(parseInt(monthlyCost));
-//   }
-// }
+function delEmployee(){
+  let delEl = event.target;
 
-function numDisplay(num) {
-  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  
+
+  $(delEl).parent().parent().remove();
+  refreshMonthlyCost();
+}
+
+function refreshMonthlyCost(){
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2
+  });
+  let monthlyCost = 0;
+  for (let i = 0; i < arrEmployee.length; i++) {
+    let monthlySalary = parseFloat(arrEmployee[i].salary / 12);
+    monthlyCost += monthlySalary;
+    $('#monthly').text(formatter.format(monthlyCost));
+  }
 }
