9c1aee264bdfb46b906f24680faba7bb4f1eb35d
Levi <levicarlson123@gmail.com>
2020-05-23 23:05:41 -0500

    all done! time to add more style

--- client.js
+++ client.js
@@ -4,40 +4,50 @@ function ready(){
   console.log('js and jq ready!');
   clickHandler();
 }
-
+// array of employee objects and monthly cost
 let arrEmployee = [];
 let monthlyCost = 0;
 
 function clickHandler(){
+  // handles when the submit, clear, and delete buttons are clicked
   $('#submit').on('click', addEmployee);
   $('#clear').on('click', function () {
     $('tbody').empty();
     arrEmployee = [];
-    $('#monthly').text('$0');
+    $('#monthly').text('$0.00');
   });
-  $('table').on('click', '.delete', delEmployee);
+  $('#data').on('click', '.delete', function () {
+    delEmployee();
+    refreshMonthlyCost();
+  });
 }
 
 function addEmployee(){
   // declares variables with the value of the text inputs
-  let first = $('#first').val();
-  let last = $('#last').val();
-  let id = Number($('#id').val());
-  let title = $('#title').val();
-  let salary = Number($('#salary').val());
+  const first = $('#first').val();
+  const last = $('#last').val();
+  const id = Number($('#id').val());
+  const title = $('#title').val();
+  const salary = Number($('#salary').val());
   // declares variables with elements to be appended
-  let elTr = $('<tr></tr>');
-  let elFirst = $(`<td>${first}</td>`);
-  let elLast = $(`<td>${last}</td>`);
-  let elId = $(`<td>${id}</td>`);
-  let elTitle = $(`<td>${title}</td>`);
-  let elSalary = $(`<td>${salary}</td>`);
-  let elDelBtn = $(`<td><button class="delete">Delete</button></td>`);
-
-  // I love this if statement
+  const elTr = $('<tr></tr>');
+  const elFirst = $(`<td>${first}</td>`);
+  const elLast = $(`<td>${last}</td>`);
+  const elId = $(`<td>${id}</td>`);
+  const elTitle = $(`<td>${title}</td>`);
+  const elSalary = $(`<td>${salary}</td>`);
+  const elDelBtn = $(`<td class="delete">x</td>`);
+  // I love this if statement :)
   if(!first || !last || !id || !title || !salary){
     console.log('Error! Inputs cannot be empty.');
     return;
+  }
+  // checks if the input id is already used on the table. if so, return early
+  for(let i of arrEmployee){
+    if(id === i.id){
+      console.log('Error! Employee with that ID already exists.');
+      return;
+    }
   }
 
   arrEmployee.push({
@@ -66,23 +76,36 @@ function addEmployee(){
 }
 
 function delEmployee(){
-  let delElIndex = $(event.target).closest('tbody tr').index();
+  // targets the table row closest to the button clicked
+  const delElIndex = $(event.target).closest('tbody tr').index();
   arrEmployee.splice(delElIndex, 1);
-
-  $(event.target).closest('tbody tr').remove();
-  refreshMonthlyCost();
+  // fades then removes the table row closest to the clicked button
+  $(event.target).closest('tbody tr').fadeOut(500, function(){$(this).remove()});
 }
 
 function refreshMonthlyCost(){
+  // checks if the array is empty and if it is, return early
+  if (arrEmployee.length === 0) {
+    $('#monthly').text('$0.00');
+    $('#monthly').parent().css('background-color', '#008000');
+    return;
+  }
+  // sets up the formatting for the #monthly element
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
     minimumFractionDigits: 2
   });
   monthlyCost = 0;
+  // loops through all employee objects in the array and adds them to monthlyCost
   for(let employee of arrEmployee){
     let monthlySalary = parseFloat(employee.salary / 12);
     monthlyCost += monthlySalary;
+    if(monthlyCost >= 20000){
+      $('#monthly').parent().css('background-color', '#ff0000');
+    } else {
+      $('#monthly').parent().css('background-color', '#008000');
+    }
     $('#monthly').text(formatter.format(monthlyCost));
   }
 }
